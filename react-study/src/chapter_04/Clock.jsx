9ea85176cc6e185fcc074578c60334ae96333af1
swkimm <swkim7161@kakao.com>
2023-12-08 15:14:53 +0900

    basic react

---
+++ react-study/src/chapter_04/Clock.jsx
@@ -0,0 +1,13 @@
+// rsc
+import React from "react";
+
+const Clock = () => {
+  return (
+    <div>
+      <h1>안녕 리액트</h1>
+      <h2>현재 시간: {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+};
+
+export default Clock;
---
+++ react-study/src/chapter_05/Comment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Comment = () => {
+  return (
+    <div>
+      <h1>처음 만든 첫 컴포턴트</h1>
+    </div>
+  );
+};
+
+export default Comment;
