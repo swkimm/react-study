f2f1a577b4bd97b28e4da72f3a9afde93213cc24
swkimm <swkim7161@kakao.com>
2023-12-11 17:32:13 +0900

    final study react

--- react-study/src/index.js
+++ react-study/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Accommodate from "./\bchapter_07/Accommodate";
+import ConfirmButton from "./chapter_08/confirmbutton";
 // import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButton />
   </React.StrictMode>,
   document.getElementById("root")
 );
